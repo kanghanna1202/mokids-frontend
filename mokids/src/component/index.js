dbf1b37e588defc2043b8f16303472af649129ad
hanna <namnam1178@daum.net>
2021-04-14 19:59:01 +0900

    [ADD] Project settings

---
+++ mokids/src/component/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import {BrowserRouter} from "react-router-dom";
+import Login from "./login/login";
+
+function Root(){
+    return <BrowserRouter></BrowserRouter>;
+}
+
+export default Root;
