11b312ae85a5ab65ec658f890eac5f4b86e51096
hanna <namnam1178@daum.net>
2021-05-18 20:27:40 +0900

    [ADD] Add movie detail page

--- mokids/src/component/index.js
+++ mokids/src/component/index.js
@@ -4,6 +4,7 @@ import Login from "./login/login";
 import Register from "./register/register";
 import Main from "./main";
 import GenreMovie from "./genreMovie";
+import MovieDetail from "./movieDetail";
 
 function Root(){
     return (
@@ -13,6 +14,7 @@ function Root(){
             <Route exact path="/register" component={Register}></Route>
             <Route exact path="/main" component={Main}></Route>
             <Route exact path="/genre/:name" component={GenreMovie}></Route>
+            <Route exact path="/movie/:id" component={MovieDetail}></Route>
         </Switch>
     </BrowserRouter>);
 }
--- mokids/src/component/movieDetail/index.js
+++ mokids/src/component/movieDetail/index.js
@@ -3,17 +3,17 @@ import "./style.css";
 import {getRequest} from "../../api";
 import logo from "../../asset/logo/logo.001.png";
 
-function GenreMovie({match}){
+function MovieDetail({match}){
     const [movieData, setMovieData] = useState([]);
     useEffect(()=>{}, [])
-    getRequest().post("/genre/", {genre:match.params.name}).then((res)=>{setMovieData(res.data)});
-    console.log(match)
+    getRequest().get(`/movie/${match.params.id}`).then((res)=>{setMovieData(res.data)});
+    console.log("efkernfkjernfkenrfknkjenkrjfkerjnfkejnrf")
     return (
         <>
         <div className="mainContainer">
             <header><img src={logo}></img></header>
-            <h2>{match.params.name}</h2>
-            <div className="genreMovie">
+            <h2>영화 세부정보</h2>
+            <div className="movieDetail">
                 {movieData.map((e,i)=><img className="poster"></img>)}
             </div>
         </div>
@@ -21,4 +21,4 @@ function GenreMovie({match}){
     );
 }
 
-export default GenreMovie
+export default MovieDetail
